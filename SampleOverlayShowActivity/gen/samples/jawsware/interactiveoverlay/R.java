fdcea3dcfda232e48d32da3cd9f4271993b4b689
Kaan Çelen <bkaancelen@gmail.com>
2014-06-05 10:23:23 +0300

    İcon boyutları ayarlandı Ekran 7" yapıldı

--- SampleOverlayShowActivity/gen/samples/jawsware/interactiveoverlay/R.java
+++ SampleOverlayShowActivity/gen/samples/jawsware/interactiveoverlay/R.java
@@ -27,11 +27,11 @@ public final class R {
         public static final int backCam=0x7f06001a;
         public static final int back_camera_preview=0x7f060000;
         public static final int camOption=0x7f060019;
-        public static final int cameraOptionImg=0x7f060005;
+        public static final int cameraOptionImg=0x7f060004;
         public static final int camera_preview=0x7f06001c;
         public static final int cloudOptionImg=0x7f060006;
         public static final int durationOption=0x7f060014;
-        public static final int durationOptionImg=0x7f060004;
+        public static final int durationOptionImg=0x7f060005;
         public static final int frontCam=0x7f06001b;
         public static final int ftpDefault=0x7f06000b;
         public static final int ftpOK=0x7f06000a;
@@ -43,9 +43,9 @@ public final class R {
         public static final int ftpUsername=0x7f06000c;
         public static final int ftpUsernameLabel=0x7f06000d;
         public static final int imageButton1=0x7f060003;
-        public static final int imageView1=0x7f060001;
+        public static final int imageView1=0x7f060002;
         public static final int imageView2=0x7f060007;
-        public static final int imageView3=0x7f060002;
+        public static final int imageView3=0x7f060001;
         public static final int minute10=0x7f060018;
         public static final int minute2=0x7f060016;
         public static final int minute5=0x7f060017;
